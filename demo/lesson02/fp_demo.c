97f18a73a57c87f1ef9d5280a9a7975f234317f5
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-03-12 17:33:33 +0700

    Bài 2 sử dụng con trỏ hàm (hàm khái quát)

--- demo/lesson02/fp_demo.c
+++ demo/lesson02/fp_demo.c
@@ -4,10 +4,16 @@
 //    như được định nghĩa ở
 //    https://mathworld.wolfram.com/OddNumber.html
 //    số có dạng 2k + 1
+int abs_i(int value) {
+  if (value < 0) {
+    return -value;
+  }
+  return value;
+}
 int count_odd(int n, int a[]) {
   int cc = 0;
   for (int i = 0; i < n; ++i) {
-    if (a[i] % 2 == 1) {  // Đếm vậy đúng hay sai
+    if (abs_i(a[i]) % 2 == 1) {  // Đếm vậy đúng hay sai
       ++cc;
     }
   }
@@ -15,15 +21,55 @@ int count_odd(int n, int a[]) {
 }
 
 // 2. Đếm số lượng số chẵn
+int count_even(int n, int a[]) {
+  int cc = 0;
+  for (int i = 0; i < n; ++i) {
+    if (abs_i(a[i]) % 2 == 0) {  // Đếm vậy đúng hay sai
+      ++cc;
+    }
+  }
+  return cc;
+}
 
 // 3. Đếm số lượng số chia hết cho 3
+int count_divided3(int n, int a[]) {
+  int cc = 0;
+  for (int i = 0; i < n; ++i) {
+    if (abs_i(a[i]) % 3 == 0) {  // Đếm vậy đúng hay sai
+      ++cc;
+    }
+  }
+  return cc;
+}
 
 // 4. Đếm số lượng số âm
+int count_negative(int n, int a[]) {
+  int cc = 0;
+  for (int i = 0; i < n; ++i) {
+    if (a[i] < 0) {  // Đếm vậy đúng hay sai
+      ++cc;
+    }
+  }
+  return cc;
+}
 
 // 5. Đếm số lượng số dương
+int count_positive(int n, int a[]) {
+  int cc = 0;
+  for (int i = 0; i < n; ++i) {
+    if (a[i] > 0) {  // Đếm vậy đúng hay sai
+      ++cc;
+    }
+  }
+  return cc;
+}
 
 int main() {
   int a[] = { -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8};
   int n = sizeof(a) / sizeof(a[0]);
   printf("Số lượng số lẻ bằng %d\n", count_odd(n, a));
+  printf("Số lượng số chẵn bằng %d\n", count_even(n, a));
+  printf("Số lượng số chia hết cho 3 bằng %d\n", count_divided3(n, a));
+  printf("Số lượng số âm bằng %d\n", count_negative(n, a));
+  printf("Số lượng số dương bằng %d\n", count_positive(n, a));
 }
--- demo/lesson02/fp_demo_generic.c
+++ demo/lesson02/fp_demo_generic.c
@@ -1,1 +1,67 @@
-// triển khai khái quát của fp_demo
+#include <stdio.h>
+
+// 1. Đếm số lượng số lẻ
+//    như được định nghĩa ở
+//    https://mathworld.wolfram.com/OddNumber.html
+//    số có dạng 2k + 1
+int abs_i(int value) {
+  if (value < 0) {
+    return -value;
+  }
+  return value;
+}
+int is_odd(int value) {
+  if (abs_i(value) % 2 == 1) {
+    return 1;
+  }
+  return 0;
+}
+
+int is_even(int value) {
+  if (abs_i(value) % 2 == 0) {
+    return 1;
+  }
+  return 0;
+}
+
+int is_divided3(int value) {
+  if (abs_i(value) % 3 == 0) {
+    return 1;
+  }
+  return 0;
+}
+
+int is_negative(int value) {
+  if (value < 0) {
+    return 1;
+  }
+  return 0;
+}
+int is_positive(int value) {
+  if (value > 0) {
+    return 1;
+  }
+  return 0;
+}
+
+typedef int (*predicate_t)(int);
+
+int count_if(int n, int a[], predicate_t p) {
+  int cc = 0;
+  for (int i = 0; i < n; ++i) {
+    if (p(a[i])) {  // Đếm vậy đúng hay sai
+      ++cc;
+    }
+  }
+  return cc;
+}
+
+int main() {
+  int a[] = { -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8};
+  int n = sizeof(a) / sizeof(a[0]);
+  printf("Số lượng số lẻ bằng %d\n", count_if(n, a, is_odd));
+  printf("Số lượng số chẵn bằng %d\n", count_if(n, a, is_even));
+  printf("Số lượng số chia hết cho 3 bằng %d\n", count_if(n, a, is_divided3));
+  printf("Số lượng số âm bằng %d\n", count_if(n, a, is_negative));
+  printf("Số lượng số dương bằng %d\n", count_if(n, a, is_positive));
+}
