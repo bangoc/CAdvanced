f1bbaf686b8d35d9f6bc3119d5c75e64ecc7c3b7
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-03-18 20:48:13 +0700

    Áp dụng phong cách code cho tệp qsort.c

--- mylib/qsort.c
+++ mylib/qsort.c
@@ -1,25 +1,19 @@
 #include "DebugPrintf/debug_printf.h"
 
 /*Displays the array, passed to this method*/
-void display(int arr[], int n)
-{
-
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-
-    printf("\n");
+void display(int arr[], int n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
 }
 
 /*Swap function to swap two values*/
-void swap(int *first, int *second)
-{
-
-    int temp = *first;
-    *first = *second;
-    *second = temp;
+void swap(int *first, int *second) {
+  int temp = *first;
+  *first = *second;
+  *second = temp;
 }
 
 /*Partition method which selects a pivot
@@ -29,28 +23,18 @@ void swap(int *first, int *second)
   lower --- lower index
   upper --- upper index
 */
-int partition(int arr[], int lower, int upper)
-{
-    printf("lower = %d  upper = %d\n", lower, upper);
-    int i = (lower - 1);
-
-    int pivot = arr[upper]; // Selects last element as the pivot value
-
-    int j;
-    for (j = lower; j < upper; j++)
-    {
-
-        if (arr[j] <= pivot)
-        { // if current element is smaller than the pivot
-
-            i++; // increment the index of smaller element
-            swap(&arr[i], &arr[j]);
-        }
+int partition(int arr[], int lower, int upper) {
+  int i = (lower - 1);
+  int pivot = arr[upper]; // Selects last element as the pivot value
+  int j;
+  for (j = lower; j < upper; j++) {
+    if (arr[j] <= pivot) { // if current element is smaller than the pivot
+      i++; // increment the index of smaller element
+      swap(&arr[i], &arr[j]);
     }
-
-    swap(&arr[i + 1], &arr[upper]); // places the last element i.e, the pivot to its correct position
-
-    return (i + 1);
+  }
+  swap(&arr[i + 1], &arr[upper]); // places the last element i.e, the pivot to its correct position
+  return (i + 1);
 }
 
 /*This is where the sorting of the array takes place
@@ -58,18 +42,12 @@ int partition(int arr[], int lower, int upper)
   lower --- Starting index
   upper --- Ending index
 */
-void quickSort(int arr[], int lower, int upper)
-{
-
-    if (upper > lower)
-    {
-
-        // partitioning index is returned by the partition method , partition element is at its correct poition
-
-        int partitionIndex = partition(arr, lower, upper);
-
-        // Sorting elements before and after the partition index
-        quickSort(arr, lower, partitionIndex - 1);
-        quickSort(arr, partitionIndex + 1, upper);
-    }
+void quickSort(int arr[], int lower, int upper) {
+  if (upper > lower) {
+    // partitioning index is returned by the partition method , partition element is at its correct poition
+    int partitionIndex = partition(arr, lower, upper);
+    // Sorting elements before and after the partition index
+    quickSort(arr, lower, partitionIndex - 1);
+    quickSort(arr, partitionIndex + 1, upper);
+  }
 }
