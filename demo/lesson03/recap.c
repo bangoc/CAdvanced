b8a9c9ac80bc3e915b7c8cceeb675a47ac71f580
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-03-19 10:17:08 +0700

    Bài 3 - Demo 1

---
+++ demo/lesson03/recap.c
@@ -0,0 +1,37 @@
+// Hiển thị bất kỳ kiểu dữ liệu nào
+
+typedef enum {
+  int_tag,
+  double_tag,
+  string_tag
+} Tags;
+
+struct {
+  Tags tag;
+  void (*handler)(void*);
+} displays[] = {{int_tag, display_i},
+                {double_tag, display_d},
+                {string_tag, display_s}};
+
+void display(void *data, Tags tag) {
+  int n = sizeof(displays)/sizeof(displays[0]);
+  for (int i = 0; i < n; ++i) {
+    if (tag == displays[i].tag) {
+      displays[i].handler(data);
+      break;
+    }
+  }
+}
+
+// Có thể tiếp tục phát triển đếm bất kỳ mảng nào?
+typedef int (*predicate_t)(int);
+
+int count_if(int n, int a[], predicate_t p) {
+  int cc = 0;
+  for (int i = 0; i < n; ++i) {
+    if (p(a[i])) {  // Đếm vậy đúng hay sai
+      ++cc;
+    }
+  }
+  return cc;
+}
---
+++ demo/lesson03/vector_demo.c
@@ -0,0 +1,15 @@
+#include "c-vector/cvector.h"
+#include "DebugPrintf/debug_printf.h"
+
+int main() {
+  cvector_vector_type(int) v = NULL;
+  cvector_push_back(v, 5);
+  cvector_push_back(v, 10);
+  cvector_push_back(v, 20);
+
+  printfDebug("v size = %d", cvector_size(v));
+  for (int i = 0; i < cvector_size(v); ++i) {
+    printfDebug("v[%d] = %d", i, v[i]);
+  }
+  return 0;
+}
