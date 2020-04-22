5aa25d9dafc4974c198fe3eaafac648377c955a2
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-04-22 22:00:35 +0700

    Bài 8: Phần 2 - igraph

---
+++ bin/igraph.cmd
@@ -0,0 +1,9 @@
+# Hướng dẫn chuẩn bị igraph
+#
+# Tải về: wget https://github.com/igraph/igraph/releases/download/0.8.1/igraph-0.8.1.tar.gz
+# Giải nén: tar -zxvf igraph-0.8.1.tar.gz
+#
+#1-Sinh Makefile: ./configure --includedir=$HOME/C/CAdvanced --libdir=$HOME/C/CAdvanced/binaries --prefix=$HOME/C/CAdvanced --enable-debug
+#2-Biên dịch: make
+#3-Cài đặt: make install
+#4-Xóa cài đặt: make uninstall
---
+++ demo/lesson08/graph_basics.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include "igraph/igraph.h"
+
+void print_edges(igraph_vector_t *v) {
+  printf("Danh sách các cạnh là:\n");
+  for (long int i = 0; i < igraph_vector_size(v)/2; ++i) {
+    printf("%li %li\n", (long int)VECTOR(*v)[2 * i],
+                        (long int)VECTOR(*v)[2 * i + 1]);
+  }
+  printf("\n");
+}
+
+int main() {
+  igraph_t g;
+  igraph_small(&g, 0, IGRAPH_DIRECTED,
+                0, 1,
+                1, 2,
+                2, 3,
+                -1);
+  printf("Số lượng đỉnh: %d \n", igraph_vcount(&g));
+  printf("Số lượng cạnh: %d\n", igraph_ecount(&g));
+  igraph_vector_t v;
+  igraph_vector_init(&v, 0);
+  igraph_get_edgelist(&g, &v, 0);
+  printf("#elements of v: %li\n", igraph_vector_size(&v));
+  print_edges(&v);
+  igraph_vector_destroy(&v);
+  igraph_destroy(&g);
+}
